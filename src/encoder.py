31d83c34a9fea1f80d93eb8cce7303cd6e036f9e
Ritwik Srinivas <ritwikrsrinivas@gmail.com>
2024-11-11 14:04:35 -0800

    initial working encoder version

--- src/encoder.py
+++ src/encoder.py
@@ -1,208 +1,256 @@
-import pysam
+"""
+Develops a one-hot encoding of a nucleotide sequence input matrix and
+RNA-seq output matrix describing the start, end, and transition states of
+reads within a specified BED window.
+"""
+# pylint: disable=fixme, disable=no-member
+
 import numpy as np
-from Bio import SeqIO
 import pandas as pd
-import pytest
+from sklearn.preprocessing import OneHotEncoder
+
+from Bio import SeqIO
+import pyranges as pr
+import pysam
 
 class Encoder:
     """
-    The purpose of this class is to develop one-hot encoding for 
-    nucleotide sequences and cigar strings of fasta and any number
-    of bam files within bed windows, all extracted from respective files. 
+    A class to generate one-hot encodings of nucleotide sequences and
+    transition state matrices of RNA-seq data based on reads within specified
+    BED windows.
+
+    Attributes:
+    -----------
+    bed_files : list of str
+        Paths to BED files specifying genomic regions.
+    fasta_file : str
+        Path to the FASTA file containing genomic sequences.
+    bam_files : list of str
+        Paths to BAM files containing RNA-seq read alignments.
+    bed_windows : pd.DataFrame
+        DataFrame holding parsed BED window information.
+    onehot_sequence_matrices : list of np.ndarray
+        List of one-hot encoded matrices for sequences.
+    transition_matrices : list of np.ndarray
+        List of transition matrices for read alignments.
     """
 
-    def __init__(self):
-        self.bed_window_list = []
-        self.one_hot_dict = {nt:pos for pos, nt in enumerate("ACGT")}
-        self.transition_dict = {}
-        self.edge_list = [('start','M'), ('M','M'), ('M','N'), ('N','N'), ('N','M'), ('M','end')]
-        self.edge_dict = {transition:i for i, transition in enumerate(self.edge_list)}
-        self.transition_matrices = []
-        
-    def extract_exon_sequences_from_fasta(self, fasta_file, bed_file):
+    transition_states = [('S','M'), ('M','M'), ('M','E'), ('M','N'), ('N','M'), ('N','N')]
+    transition_dict = {transition: i for i, transition in enumerate(transition_states)}
+
+    def __init__(self, bed_files: list[str], fasta_file: str, bam_files: list[str]):
+
+        # TODO: throw error if all of these files not specified?
+        self.bed_files = bed_files
+        self.fasta_file = fasta_file    # TODO: support multiple fasta files?
+        self.bam_files = bam_files      # TODO: support for SAM/CRAM files?
+
+        self.bed_windows = None         # TODO: do we care about strandedness of BED windows?
+        self.onehot_sequence_matrices = None   # TODO: convert to numpy array?
+        self.transition_matrices = None # TODO: convert to numpy array?
+
+    def extract_bed_windows(self):
         """
-        This def extracts nucleotide strings within each of the bed windows.
+        Parses BED files and loads window coordinates into a DataFrame.
 
-        Input: fasta and bed files
-        Output: dictionary of nucleotide sequences with keys of read name,window position, strand
+        Sets:
+        -----
+        self.bed_windows : pd.DataFrame
+            Contains columns: 'chrom', 'start', 'end' for each BED region.
         """
-        
-        # Parse FASTA file
-        fasta_sequences = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta"))
 
-        # Read BED file and extract sequences for each region
-        exon_sequences = {}
-        with open(bed_file, "r") as bed:
-            for line in bed:
-                # Skip empty lines or lines that don't have enough columns
-                if line.startswith("track") or len(line.strip()) == 0:
-                    continue
-                
-                parts = line.strip().split()
-                seq_id, start, end, exon_id, quality, strand = parts[:6]
-                start, end = int(start), int(end)
-                print(seq_id, strand)
-                self.bed_window_list.append((seq_id,start,end))
-                # Retrieve the sequence and slice according to the BED coordinates
-                if seq_id in fasta_sequences:
-                    exon_seq = fasta_sequences[seq_id].seq[start:end]
-                    exon_position_key = f"{exon_id},{str(start)},{str(end)},{strand}"
-                    exon_sequences[exon_position_key] = exon_seq
+        # Parse BED files to generate list of BED windows
+        bed_data = []
+        for bed_file in self.bed_files:
+            gr = pr.read_bed(bed_file)
+            for window in gr.df.itertuples():
+                # TODO: include interval.name?, handle repeat windows?
+                data = (window.Chromosome, window.Start, window.End)
+                bed_data.append(data)
 
-        return exon_sequences
-        
+        # Create DataFrame of BED windows - TODO: switch this to a list of bedparse Interval objects?
+        self.bed_windows = pd.DataFrame(bed_data, columns=['chrom', 'start', 'end'])
 
-    def bed_encoder(self, seq, reverse_flag):
+    def extract_sequence_onehot(self):
         """
-        This def develops one-hot encoding for the nucleotide sequences within each bed window. 
+        Extracts nucleotide sequences from FASTA file for each BED window and
+        generates one-hot encodings.
 
-        Input:nucleotide sequences, strand boolean flag
-        Output: one-hot encoded nucleotide sequence numpy matrix
+        Sets:
+        -----
+        self.onehot_sequence_matrices : list of np.ndarray
+            One-hot encoded matrices for each BED window's sequence.
         """
-        transition_matrix = np.zeros((8, len(seq)), dtype=int)
-    
-        x = 4 if reverse_flag else 0
-        for i, nt in enumerate(seq):
-            transition_matrix[self.one_hot_dict[nt] + x, i] = 1
-    
-        return transition_matrix
-        
 
-    def generate_cigar_transitions(self, cigar_tuples, reverse_flag):
+        onehot_sequences_matrices = [] # TODO: convert to numpy array?
+        categories = [['A', 'T', 'C', 'G']]
+        encoder = OneHotEncoder(handle_unknown='ignore', categories=categories, sparse_output=False)
+
+        # Parse FASTA file - TODO: to_dict() is non-memory friendly - find alternate solution?
+        fasta_sequences = SeqIO.to_dict(SeqIO.parse(self.fasta_file, 'fasta'))
+
+        # Iterate over DataFrame of BED windows
+        for bed_window in self.bed_windows.itertuples():
+
+            # Retrieve nucleotide sequence defined by BED coordinates
+            if bed_window.chrom in fasta_sequences:  # TODO: will our FASTA file have sequences named by chromosome?
+                nucleotide_sequence = np.array(fasta_sequences[bed_window.chrom].seq[bed_window.start:bed_window.end]) # TODO: do we care about strandedness of BED windows?
+
+                # One-Hot encode nucleotide sequence
+                nucleotide_sequence = nucleotide_sequence.reshape((-1, 1))
+                onehot_sequence_matrix = encoder.fit_transform(nucleotide_sequence)
+                onehot_sequences_matrices.append(onehot_sequence_matrix)
+
+            # TODO: handle case where BED window sequence not found in FASTA file
+
+        self.onehot_sequence_matrices = onehot_sequences_matrices
+
+    def extract_transition_matrices(self):
         """
-        Generates a list of transitions from a CIGAR tuple list.
-        Each transition tuple contains the previous and current CIGAR operations,
-        with 'start' and 'end' markers at the beginning and end of the list.
+        Constructs transition matrices based on read alignments in BAM files
+        for each BED window.
 
-        Input: list of cigar tuples from each bam alignment, flag boolean 
-        Output: list of edges which depict the transition path of each CIGAR string
+        Sets:
+        -----
+        self.transition_matrices : list of np.ndarray
+            Transition matrices capturing state changes for reads per BED window.
         """
-        # Dictionary to map CIGAR operation codes to symbols
-        cigar_dict = {0: "M", 1: "I", 2: "D", 3: "N", 4: "S", 5: "H", 6: "P", 7: "=", 8: "X"}
 
-        # Convert CIGAR tuples into a flat list of states
-        state_list = []
-        for enum, length in cigar_tuples:
-            state_char = cigar_dict[enum]
-            state_list.extend([state_char] * length)
+        transition_matrices = [] # TODO: convert to numpy array?
 
-        # Initialize the transition list with the start marker
-        transitions = []
-        # Generate the transition list as tuples of (previous_state, current_state)
-        previous_state = state_list[0]
-        
-        for current_state in state_list[1:]:
-            transitions.append((previous_state, current_state))
-            previous_state = current_state
+        # Iterate over DataFrame of BED windows
+        for window in self.bed_windows.itertuples():
 
-        #adding start and end edges
-        finished_transition_list = [('start', state_list[0])]
-        if reverse_flag: 
-            finished_transition_list.extend(transitions[::-1])
-            finished_transition_list.append((previous_state, 'end'))
-            return finished_transition_list
-        else:
-            finished_transition_list.extend(transitions)
-            finished_transition_list.append(((previous_state, 'end')))
-            return finished_transition_list
-    
-    def overlap_detector(self, bam_list):
+            matrix_shape = (12, window.end - window.start)
+            transition_matrix = np.zeros(matrix_shape, dtype=int)
+
+            # Iterate over BAM files
+            for bam_file in self.bam_files: # TODO: stipulate that bam files are split by chromosome (for efficiency)?
+
+                # Iterate over reads within BED window (even partially) - TODO: use pysam pileup instead?
+                samfile = pysam.AlignmentFile(bam_file, 'rb')
+                for read in samfile.fetch(window.chrom, window.start, window.end - 1): # BED window end coordinate is not inclusive, samfile region is inclusive, TODO: will our BAM file have reads named by chromosome?
+                    self.cigar_transition_features(transition_matrix, window.start, window.end, read)
+
+            transition_matrices.append(transition_matrix)
+
+        # TODO: save these to disk instead
+        self.transition_matrices = transition_matrices
+
+    # TODO: better name for this function
+    def cigar_transition_features(self, transition_matrix: np.array, window_start: int, window_end: int, read: pysam.AlignedRead):
         """
-        This def determines if bam alignments exist within the bed windows contained in the bed_window_list.
-        Should handle bam alignments which are subsets of bed windows, and bed windows which are subsets of bam alignments. 
+        Updates transition matrix with CIGAR-based state changes for a given read.
 
-        Input: list of bam files
-        Output: dictionary of state transition paths recorded as lists of edge tuples
+        Parameters:
+        -----------
+        transition_matrix : np.ndarray
+            Matrix tracking state transitions.
+        window_start : int
+            Start coordinate of the BED window.
+        window_end : int
+            End coordinate of the BED window.
+        read : pysam.AlignedSegment
+            BAM alignment data for a given read.
         """
-    #iterate through the list of N bam files
-        for bam_file in bam_list:
-            bam_file = pysam.AlignmentFile(bam_file, "rb")
 
-            # Iterate through each read in the BAM file
-            for read in bam_file:
-                read_chrom = read.reference_name
-                read_start = read.reference_start
-                read_end = read.reference_end
-                #flag for reverse
-                reverse_flag = read.is_reverse
-            
-                #associate bed windows and bam alignments
-                for chrom, start, end in self.bed_window_list:
-                    if chrom == read_chrom and start <= read_start <= end and start <= read_end <= end:
-                        cigar_tuples_list = read.cigartuples
-                        if reverse_flag:
-                            self.transition_dict[str(read).split("\t")[0]+'_reverse'] = self.generate_cigar_transitions(cigar_tuples_list, reverse_flag)
-                        else:
-                            self.transition_dict[str(read).split("\t")[0]] = self.generate_cigar_transitions(cigar_tuples_list, reverse_flag)
+        # Convert pysam's list of CIGAR tuples (run-length encoded splice states) to list of
+        # transition states. Also: convert to standard CIGAR codes and ensure cigar_tuples contain
+        # only "M" (0) or "N" (3) transition states - TODO: handle other cigar strings?
+        cigar_tuples = [('S', 'M')] # TODO: better name for this
+        for cigar_tuple in read.cigartuples:
+            if cigar_tuple[0] == 0:
+                cigar_tuples.extend([('M', cigar_tuple[1] - 1), ('M', 'N')])
+            elif cigar_tuple[0] == 3:
+                cigar_tuples.extend([('N', cigar_tuple[1] - 1), ('N', 'M')])
+            else:
+                return
+        cigar_tuples[-1] = ('M', 'E')
 
-    def transitioner(self):
+        # Line up read to BED window
+        read_offset = read.reference_start - window_start
+        cigar_tuples, read_offset = self.trim_cigar_tuples(cigar_tuples, read_offset)
+
+        # Add transition features as described by cigar_tuples
+        reverse_offset = 6 if read.is_reverse else 0
+        features_added = 0
+        for cigar_tuple in cigar_tuples:
+
+            # TODO: improve this code somewhat
+            if not isinstance(cigar_tuple[1], int): # if non-self-loop transition (no repetition)
+                transition_feat = self.transition_dict[(cigar_tuple[0], cigar_tuple[1])]
+                if window_start + read_offset + features_added < window_end:
+                    transition_matrix[transition_feat + reverse_offset][read_offset + features_added] += 1
+                else:
+                    return
+                features_added += 1
+
+            else: # self-loop condition (repetition)
+                transition_feat = self.transition_dict[(cigar_tuple[0], cigar_tuple[0])]
+                for _ in range(cigar_tuple[1]):
+                    if window_start + read_offset + features_added < window_end:
+                        transition_matrix[transition_feat + reverse_offset][read_offset + features_added] += 1
+                    else:
+                        return
+                    features_added += 1
+
+    def trim_cigar_tuples(self, cigar_tuples: list[tuple[str, str | int]], read_offset: int) -> tuple[list[tuple[str, str | int]], int]:
         """
-        This def develops one-hot encoded numpy matrices from CIGAR state transitions paths.
+        Adjusts CIGAR tuples if the read starts before the BED window.
 
-        Input:dictionary of transition paths
-        Output:one-hot encoded matrix of 
-        """
-        for key in self.transition_dict.keys():
-            transition_list = self.transition_dict[key]
-            print(transition_list)
-            # Create a new matrix for each alignment
-            transition_matrix = np.zeros((12, len(transition_list)), dtype=int)
+        Parameters:
+        -----------
+        cigar_tuples : list of tuple of (str, str | int)
+            CIGAR tuples corresponding to a particular read.
+        read_offset : int
+            Offset of read alignment relative to the window start.
 
-            print(transition_matrix.shape)        
-            # Encode transitions for this alignment
-            x = 0 if 'reverse' not in key else 6
-            for pos, transition in enumerate(transition_list):
-                if transition in self.edge_dict:
-                    transition_matrix[self.edge_dict[transition] + x, pos] = 1
-    
-            #transition_tensor = torch.tensor(transition_matrix)
-            #transition_matrices.append(transition_tensor)
-    
-            df = pd.DataFrame(transition_matrix, index=self.edge_list*2, columns=[f"Pos_{j+1}" for j in range(transition_matrix.shape[1])])
-            print(df)
-            csv_filename = f"transition_matrix{key}.csv"
-    
-    def driver(self):
-        """Driver function to manage class functon calls.
-        
-        Input:
-        self.extract_exon_sequences_from_fasta("test_seq.fa", "test_reads.bed")
-        self.bed_encoder(seq, reverse_flag)
-        self.overlap_detector(['test_reads.bam', 'test_reads.bam'])
-        self.transitioner()
-        
-        Output: function calls
+        Returns:
+        --------
+        list of tuple of (str, str | int)
+            Adjusted CIGAR tuples.
+        int
+            Updated offset of read alignment corresponding to adjusted CIGAR tuples.
         """
-        
-        bed_seq_dict = self.extract_exon_sequences_from_fasta("test_seq.fa", "test_reads.bed")
-        for key in bed_seq_dict:
-            seq = bed_seq_dict[key]
-            print(seq)
-            reverse_flag = False
-            if '-' in key:
-                reverse_flag = True
-                #for reverse alignments, should the requence be the RC? 
-                seq = seq.lower().replace('a','T').replace('t','A').replace('g','C').replace('c','G')[::-1]
-                print(reverse_flag)
-    
-        encoded_seq = self.bed_encoder(seq, reverse_flag)
-        
-        print(key, len(encoded_seq))
-        print(encoded_seq)
-        
-        self.overlap_detector(['test_reads.bam', 'test_reads.bam'])
-        self.transitioner()
-    
-    @staticmethod
-    def pytest_test_method():
-        return True
+
+        # "Fast-forward" the CIGAR tuple encoding of reads that begin before the BED window
+        i = 0 # keep track of count of processed tuples, to be sliced out later
+        # TODO: improve this code somewhat
+        while read_offset < 0:
+            repetition = cigar_tuples[i][1]
+            if not isinstance(repetition, int): # if non-self-loop transition (no repetition)
+                read_offset += 1
+                i += 1
+                continue
+            if read_offset + repetition <= 0:
+                read_offset += repetition
+                i += 1
+                continue
+            cigar_tuples[i] = (cigar_tuples[i][0], cigar_tuples[i][1] + read_offset)
+            read_offset = 0
+
+        return cigar_tuples[i:], read_offset
+
+    def encode(self):
+        """Driver method to orchestrate encoding processes."""
+
+        self.extract_bed_windows()
+        self.extract_sequence_onehot()
+        self.extract_transition_matrices()
 
 def main():
     """Access class and call driver member function to begin programatic cascade."""
 
-    class_access = Encoder()
-    class_access.driver()
+    bed_files = []
+    fasta_file = ''
+    bam_files = []
 
+    encoder = Encoder(bed_files, fasta_file, bam_files)
+    encoder.encode()
+
+    for i, matrices in enumerate(zip(encoder.onehot_sequence_matrices, encoder.transition_matrices)):
+        print(f'\nOne-Hot Sequence Matrix {i}:\n{matrices[0]}\n')
+        print(f'\nTransition Matrix {i}:\n{matrices[1]}\n')
 
 if __name__ == '__main__':
     main()
